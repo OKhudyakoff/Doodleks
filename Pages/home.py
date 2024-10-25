0dac490284111381f72316b1051a1d653e32a5fb
Mikhail <mseleznevdev@gmail.com>
2024-10-26 03:56:49 +0500

    сделал прототип карточки с домашней страницы

--- Pages/home.py
+++ Pages/home.py
@@ -8,7 +8,82 @@ register_page(__name__)
 
 # test_cards - тестовые карточки челенджей
 test_cards = []
-for i in range(10):
+# for i in range(10):
+#     card = dbc.Card(
+#         [
+#             dbc.Row(
+#                 [
+#                     dbc.Col(
+#                         dbc.CardImg(
+#                             src=dash.get_asset_url("static/images/portrait-placeholder.png"),
+#                             className="img-fluid rounded-start",
+#                         ),
+#                         className="col-md-4",
+#                     ),
+#                     dbc.Col(
+#                         dbc.CardBody(
+#                             [
+#                                 html.H4(f"Card title: {i+1}", className="card-title"),
+#                                 html.P(
+#                                     "This is a wider card with supporting text "
+#                                     "below as a natural lead-in to additional "
+#                                     "content. This content is a bit longer.",
+#                                     className="card-text",
+#                                 ),
+#                                 html.Small(
+#                                     "Last updated 3 mins ago",
+#                                     className="card-text text-muted",
+#                                 ),
+#                             ]
+#                         ),
+#                         className="col-md-8",
+#                     ),
+#                 ],
+#                 className="g-0 d-flex align-items-center",
+#             )
+#         ],
+#         style={
+#             "maxWidth": "70%",
+#         },
+#     )
+
+#     test_cards.append(
+#         dbc.ListGroupItem(
+#             card,
+#             className="mb-3"
+#         ),
+#     )
+
+layout = html.Div(
+    [
+        dcc.Location(id='home-url', refresh=False),
+        html.H2('Челенджи', id='header-title', className='ten columns'),
+
+        dbc.Button('Submit', id='submit-val', n_clicks=0),
+
+        html.Div(
+            dbc.ListGroup(
+                children=test_cards,
+                id="chalenge-cards",
+            ),
+            style={
+                "maxWidth":"70%",
+                "margin-left" : "auto",
+                "margin-right" : "auto"
+            }
+        ),
+    
+        
+        # html.Div(id='cards'),
+    ]
+)
+
+@callback(
+    Output('chalenge-cards', 'children'),
+    Input('submit-val', 'n_clicks'),
+    prevent_initial_call=True
+)
+def create_new_card(val):
     card = dbc.Card(
         [
             dbc.Row(
@@ -23,7 +98,7 @@ for i in range(10):
                     dbc.Col(
                         dbc.CardBody(
                             [
-                                html.H4(f"Card title: {i+1}", className="card-title"),
+                                html.H4(f"Card title: {val}", className="card-title"),
                                 html.P(
                                     "This is a wider card with supporting text "
                                     "below as a natural lead-in to additional "
@@ -49,39 +124,11 @@ for i in range(10):
 
     test_cards.append(
         dbc.ListGroupItem(
-            card,
-            className="mb-3"
-        ),
+            children=card,
+            className="mb-3",
+            href=f'/card/{val}',
+            action=True
+        )
     )
 
-layout = html.Div(
-    [
-        dcc.Location(id='home-url', refresh=False),
-        html.H2('Челенджи', id='header-title', className='ten columns'),
-
-        html.Div(
-            dbc.ListGroup(test_cards),
-            style={
-                "maxWidth":"70%",
-                "margin-left" : "auto",
-                "margin-right" : "auto"
-            }
-        )
-        # html.Div(
-        #     test_cards,
-        # ),
-        
-        # dbc.Button(id='new-card', children='myChildren', n_clicks=0, color="secondary", className="me-1", value='hihi'),
-        # dbc.Button('Submit', id='submit-val', n_clicks=0),
-        # html.Div(id='cards'),
-    ]
-)
-
-# @callback(
-#     Output('cards', 'children'),
-#     Input('submit-val', 'n_clicks'),
-#     prevent_initial_call=True
-# )
-# def create_new_card(val):
-#     return l
-
+    return test_cards
