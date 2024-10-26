30aa0214135536dfc596595f05ba8f7fb1afc6da
koksfox <mr.griboed@gmail.com>
2024-10-26 20:58:49 +0300

    nothing importantant

--- nav_buttons.py
+++ nav_buttons.py
@@ -8,13 +8,10 @@ dropdown = html.Div(
         dbc.DropdownMenu(
             [
                 dbc.DropdownMenuItem(
-                    "Мои челленджи", href="/my_challenges"
-                ),
-                dbc.DropdownMenuItem(
-                    "Настройки", href="/settings"
-                ),
-                dbc.DropdownMenuItem(
                     "Личный кабинет", href="/lk"
+                ),
+                dbc.DropdownMenuItem(
+                    "Мои челленджи", href="/my_challenges"
                 ),
                 dbc.DropdownMenuItem(
                     "Выход", id="logout-button", n_clicks=0
