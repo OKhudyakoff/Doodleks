60262b6f7deb7526e4a29c997933d8044d332ab3
Mikhail <my.rs@mail.ru>
2024-10-26 14:04:43 +0300

    simple orm

--- testapi.py
+++ testapi.py
@@ -1,16 +1,20 @@
-from APIPostgres import APIPostgres
+from api.APIPostgres import APIPostgres
 
 
 db = APIPostgres()
 
-rez = db.executeQuery("create table if not exists test (a int);")
+# rez = db.executeQuery("create table if not exists test (a int);")
 
-print(rez)
+# print(rez)
 
-rez = db.executeQuery("insert into test values (1)")
+# rez = db.executeQuery("insert into test values (1)")
 
-print(rez)
+# print(rez)
 
-rez = db.executeQuery("select * from test;")
+# rez = db.executeQuery("select * from test;")
 
-print(rez)
+# print(rez)
+
+# query = f'''select max(id) + 1 from user_'''
+
+# rez = db.executeQuery(query)
